e94b7585430ca50488137d6511c617e94cb0d09a
Anusuya Bhattacharjee <anusuya@appstango.com>
2024-09-22 14:57:41 +0530

    server&DB setup

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databseURl = process.env.DATABASE_URL;
+
+app.use(
+    cors({
+        origin: [process.env.ORIGIN],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        credentials: true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(port, ()=> {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+mongoose.connect(databseURl).then(()=> {
+    console.log("Database Connected!!")
+}).catch(err=> {
+    console.log(err.message);
+})
